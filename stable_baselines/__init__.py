e513e09ba23376a2a693195166565094af397f2b
nixieslesmana <nixies.lesmana@gmail.com>
2023-07-18 15:45:23 +0800

    Add files via upload

---
+++ stable_baselines/__init__.py
@@ -0,0 +1,35 @@
+import os
+import warnings
+
+from stable_baselines.a2c import A2C
+from stable_baselines.a2c_original import A2C_Original
+from stable_baselines.acer import ACER
+from stable_baselines.acktr import ACKTR
+from stable_baselines.deepq import DQN
+from stable_baselines.her import HER
+from stable_baselines.ppo2 import PPO2
+from stable_baselines.td3 import TD3
+from stable_baselines.sac import SAC
+
+# Load mpi4py-dependent algorithms only if mpi is installed.
+try:
+    import mpi4py
+except ImportError:
+    mpi4py = None
+
+if mpi4py is not None:
+    from stable_baselines.ddpg import DDPG
+    from stable_baselines.gail import GAIL
+    from stable_baselines.ppo1 import PPO1
+    from stable_baselines.trpo_mpi import TRPO
+del mpi4py
+
+# Read version from file
+version_file = os.path.join(os.path.dirname(__file__), "version.txt")
+with open(version_file, "r") as file_handler:
+    __version__ = file_handler.read().strip()
+
+
+warnings.warn(
+    "stable-baselines is in maintenance mode, please use [Stable-Baselines3 (SB3)](https://github.com/DLR-RM/stable-baselines3) for an up-to-date version. You can find a [migration guide](https://stable-baselines3.readthedocs.io/en/master/guide/migration.html) in SB3 documentation."
+)
