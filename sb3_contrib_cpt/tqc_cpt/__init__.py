a6c5a9abd9903e98454d4f521d28bc01dfd4a64f
nixieslesmana <nixies.lesmana@gmail.com>
2023-07-24 12:02:33 +0800

    Add files via upload

---
+++ sb3_contrib_cpt/tqc_cpt/__init__.py
@@ -0,0 +1,4 @@
+from sb3_contrib.tqc.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+from sb3_contrib_cpt.tqc_cpt.tqc_cpt import TQC_CPT
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "TQC_CPT"]
